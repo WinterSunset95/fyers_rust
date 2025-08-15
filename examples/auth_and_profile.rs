694a15e9148857974b101c5251aaca461242ad17
WinterSunset95 <wintersunset95@gmail.com>
2025-08-16 01:46:05 +0530

    fix: minor fix in examples/auth_and_profile.rs

--- examples/auth_and_profile.rs
+++ examples/auth_and_profile.rs
@@ -44,7 +44,7 @@ async fn main() -> Result<(), FyersError> {
     println!("\n Access token generated. Fetching user profile...");
 
     println!("Fetching user profile...");
-    let client = FyersClient::new(FYERS_API_BASE_URL.to_string(), client_id, access_token.clone());
+    let client = FyersClient::new(client_id, access_token.clone());
 
     match client.get_profile().await {
         Ok(profile) => {
